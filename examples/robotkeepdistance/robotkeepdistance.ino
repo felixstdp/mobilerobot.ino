6ada78711d6482cfedd61c3e7c77b9222bddd93d
fstdp <fstdp@hotmail.com>
2013-10-28 12:41:18 +0100

    Create robotkeepdistance.ino

---
+++ examples/robotkeepdistance/robotkeepdistance.ino
@@ -0,0 +1,21 @@
+#define distance 30
+
+#include <RobotMovil.h>
+RobotMovil robot;
+
+#include <NewPing.h>
+NewPing sonar(10,10,50);
+
+void setup()
+{
+}
+
+void loop()
+{
+  delay(30);
+  float dist = sonar_L.ping_cm();
+  if (dist==0) {dist=50;}
+
+  if (dist<distance) {robot.reverse(0);}
+  else {robot.forward(0);}
+}
