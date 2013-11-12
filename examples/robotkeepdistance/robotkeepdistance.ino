2282ed232bf25d67d997d377645a33e1dee59157
fstdp <fstdp@hotmail.com>
2013-11-12 22:49:06 +0100

    Update robotkeepdistance.ino

--- examples/robotkeepdistance/robotkeepdistance.ino
+++ examples/robotkeepdistance/robotkeepdistance.ino
@@ -13,9 +13,10 @@ void setup()
 void loop()
 {
   delay(30);
-  float dist = sonar_L.ping_cm();
+  int dist = sonar.ping_cm();
   if (dist==0) {dist=50;}
 
-  if (dist<distance) {robot.reverse(0);}
-  else {robot.forward(0);}
+  if (dist<distance) {robot.reverse(120,120,0);
+  if (dist>distance) {robot.forward(120,120,0);
+  if (dist==distance) {robot.brake(0)};
 }
