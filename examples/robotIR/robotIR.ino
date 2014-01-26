14b1111f70c88697f4f840e7f46c0fe435a38ec6
fstdp <fstdp@hotmail.com>
2014-01-26 10:52:14 +0100

    Create robotIR.ino

---
+++ examples/robotIR/robotIR.ino
@@ -0,0 +1,41 @@
+#include <RobotMovil.h>
+RobotMovil robot;
+
+#include <IRremote.h>
+#define vel 200
+
+IRrecv irrecv(9);
+decode_results results;
+
+void setup()
+{
+  irrecv.enableIRIn(); // Start the receiver
+}
+
+void loop() {
+  if (irrecv.decode(&results))
+  {
+    unsigned long IR=results.value;
+
+    switch(IR)
+    {
+      case 0x511DBB:
+        forward(120,120,500);
+        break;
+      case 0xA3C8EDDB:
+        reverse(120,120,500);
+        break;
+      case 0x52A3D41F:
+        rotate(500);
+        break;
+      case 0x20FE4DBB:
+        rotate(-500);
+        break;
+      case 0xD7E84B1B:
+        brake(0);
+        break;
+    }
+    brake(0);
+    irrecv.resume(); // Receive the next value
+  }
+}
