7508a05bcc0b5c12a59f98e17495f86708742846
fstdp <fstdp@hotmail.com>
2014-01-26 10:21:30 +0100

    Create robotbluetooth.ino

---
+++ examples/robotbluetooth/robotbluetooth.ino
@@ -0,0 +1,21 @@
+#include <Ardumoto.h>
+Ardumoto robot;
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  if (Serial.available()>0)
+  {
+    char btcode=Serial.read();
+    if (btcode=='F') robot.forward(100,100,500);
+    if (btcode=='B') robot.reverse(100,100,500);
+    if (btcode=='R') robot.rotate(250);
+    if (btcode=='L') robot.rotate(-250);
+    btcode='X';
+    robot.brake(0);
+  }
+}
