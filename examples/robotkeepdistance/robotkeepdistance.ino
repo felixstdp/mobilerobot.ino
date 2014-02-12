dbf179b75387e7093aea481ea09f7b29ec3c1cdf
fstdp <fstdp@hotmail.com>
2014-02-12 10:00:33 +0100

    Update robotkeepdistance.ino

--- examples/robotkeepdistance/robotkeepdistance.ino
+++ examples/robotkeepdistance/robotkeepdistance.ino
@@ -6,15 +6,17 @@ RobotMovil robot;
 #include <NewPing.h>
 NewPing sonar(10,10,50);
 
+int dist
+
 void setup() {}
 
 void loop()
 {
   delay(30);
-  int dist = sonar.ping_cm();
-  if (dist==0) {dist=50;}
+  dist = sonar.ping_cm();
+  if (dist==0) dist=50;
 
-  if (dist<limit) {robot.reverse(120,120,0);
-  if (dist>limit) {robot.forward(120,120,0);
-  if (dist==limit) {robot.brake(0)};
+  if (dist<limit) robot.reverse(120,120,0);
+  if (dist>limit) robot.forward(120,120,0);
+  if (dist==limit) robot.brake(0);
 }
