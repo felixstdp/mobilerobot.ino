904b66fac9e05a7b1062a5786dd7a7652ce9ca82
fstdp <fstdp@hotmail.com>
2014-02-12 10:03:07 +0100

    Update robotavoidobstacle.ino

--- examples/robotavoidobstacle/robotavoidobstacle.ino
+++ examples/robotavoidobstacle/robotavoidobstacle.ino
@@ -5,6 +5,8 @@ RobotMovil robot;
 NewPing sonar_L(10,10,50);
 NewPing sonar_R(11,11,50);
 
+float dist_L, dist_R, dist
+
 void setup()
 {
 }
@@ -12,19 +14,19 @@ void setup()
 void loop()
 {
   delay(30);
-  float dist_L = sonar_L.ping_cm();
+  dist_L = sonar_L.ping_cm();
   if (dist_L==0) {dist_L=50;}
 
   delay(30);
-  float dist_R = sonar_R.ping_cm();
+  dist_R = sonar_R.ping_cm();
   if (dist_R==0) {dist_R=50;}
  
-  float dist=min(dist_L,dist_R);
+  dist=min(dist_L,dist_R);
  
   if (dist<20)
   {
-    if (dist_L>dist_R) {robot.rotate(250);}
-    else {robot.rotate(-250);}
+    if (dist_L>dist_R) robot.rotate(250);
+    else robot.rotate(-250);
   }
-  else {robot.forward(100,100,0);}
+  else robot.forward(120,120,0);
 }
