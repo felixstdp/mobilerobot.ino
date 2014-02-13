b6ecb64f9fef3e8ab0901bea2aacb8c5df98b96c
fstdp <fstdp@hotmail.com>
2014-02-13 10:13:53 +0100

    Update robotbluetooth.ino

--- examples/robotbluetooth/robotbluetooth.ino
+++ examples/robotbluetooth/robotbluetooth.ino
@@ -11,11 +11,23 @@ void loop()
   if (Serial.available()>0)
   {
     char btcode=Serial.read();
-    if (btcode=='F') robot.forward(100,100,500);
-    if (btcode=='B') robot.reverse(100,100,500);
-    if (btcode=='R') robot.rotate(250);
-    if (btcode=='L') robot.rotate(-250);
-    btcode='X';
-    robot.brake(0);
+  }
+  switch (btcode)
+  {
+    case 'F':
+      robot.forward(100,100,500);
+      break;
+    case'B':
+      robot.reverse(100,100,500);
+      break;
+    case 'R':
+      robot.rotate(250);
+      break;
+    case 'L'
+      robot.rotate(-250);
+      break;
+    default:
+      robot.brake(0);
+      btcode='X';
   }
 }
