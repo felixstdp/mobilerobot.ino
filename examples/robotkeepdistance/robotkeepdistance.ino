4d6e0844238fa3a2f24fa7ae405e2389f3c67621
fstdp <fstdp@hotmail.com>
2014-02-13 10:40:11 +0100

    Update robotkeepdistance.ino

--- examples/robotkeepdistance/robotkeepdistance.ino
+++ examples/robotkeepdistance/robotkeepdistance.ino
@@ -1,10 +1,11 @@
-#define limit 15
-
 #include <RobotMovil.h>
 RobotMovil robot;
 
 #include <NewPing.h>
 NewPing sonar(10,10,50); // trigger y echo al pin 10, distancia máxima limitada a 50cm
+
+int limit=15; // límite de frenado y velocidad de avance
+int vel=120;
 
 int dist
 
@@ -16,7 +17,7 @@ void loop()
   dist = sonar.ping_cm();
   if (dist==0) dist=50;  // corrige las distancias mayores de 50cm que dan lectura 0
 
-  if (dist<limit) robot.reverse(120,120,0);
-  if (dist>limit) robot.forward(120,120,0);
+  if (dist<limit) robot.reverse(vel,vel,0);
+  if (dist>limit) robot.forward(vel,vel,0);
   if (dist==limit) robot.brake(0);
 }
