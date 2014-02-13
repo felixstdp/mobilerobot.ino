4acaa941b1a0e84bfaf8f1a06514fca807a1c74d
fstdp <fstdp@hotmail.com>
2014-02-13 10:19:07 +0100

    Update robotkeepdistance.ino

--- examples/robotkeepdistance/robotkeepdistance.ino
+++ examples/robotkeepdistance/robotkeepdistance.ino
@@ -4,7 +4,7 @@
 RobotMovil robot;
 
 #include <NewPing.h>
-NewPing sonar(10,10,50);
+NewPing sonar(10,10,50); // trigger y echo al pin 10, distancia máxima limitada a 50cm
 
 int dist
 
@@ -12,9 +12,9 @@ void setup() {}
 
 void loop()
 {
-  delay(30);
+  delay(30);  // necesario para que el sensor espere el eco del pulso
   dist = sonar.ping_cm();
-  if (dist==0) dist=50;
+  if (dist==0) dist=50;  // corrige las distancias mayores de 50cm que dan lectura 0
 
   if (dist<limit) robot.reverse(120,120,0);
   if (dist>limit) robot.forward(120,120,0);
