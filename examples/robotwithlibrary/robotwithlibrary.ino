3ecf4f2578bf7ba2a3a52c80fee57ff70f777ef3
fstdp <fstdp@hotmail.com>
2014-02-13 10:10:02 +0100

    Update robotwithlibrary.ino

--- examples/robotwithlibrary/robotwithlibrary.ino
+++ examples/robotwithlibrary/robotwithlibrary.ino
@@ -7,6 +7,8 @@ void setup()
 
 void loop()
 {
-  robot.forward(120,120,1000);
-  robot.brake(100000);
+  robot.forward(120,120,1000); // avance con velocidad rueda derecha, rueda izquierda y tiempo en ms
+  robot.reverse(120,120,1000); // idem pero retroceso
+  robot.rotate(1000); // giro durante un tiempo en ms, valores negativo o positivos indican sentido de giro
+  robot.brake(100000); // detiene el robot un tiempo
 }
